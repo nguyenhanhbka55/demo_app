8e77692722c512e9aadf09d97ef4ea09a6e435cc
nguyenhanhbka55 <thuhanh.hust@gmail.com>
2014-10-20 13:28:35 +0700

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+  def show
+  	@user = User.find(params[:id])  
+  end
+  	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Welcome to the site!"
+			#redirect_to "/"
+			render :action => 'show'
+		else
+			flash[:alert] = "There was a problem creating your account. Please try again."
+			redirect_to :back
+		end
+	end
+
+	def update
+		
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:email, :password, :password_confirmation)
+		end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+	attr_accessor :password
+	#validates_confirmation_password :password
+	EMAIl_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+	validates :email , format: { with: EMAIl_REGEX}, uniqueness: {case_sensitive: false}
+	#validates :name, :length => {:maximum => 50}, presence: true
+	validates :email, :length => {:within => 5..30}, presence: true
+	validates :password_confirmation, presence: true
+	validates :password, :confirmation => true , presence: true
+
+	before_save :encrypt_password
+	def encrypt_password
+		self.password_salt = BCrypt::Engine.generate_salt
+		self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+	end
+
+	def self.authenticate(email,password)
+		user = User.where(email: email).first
+		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+			user
+		else
+			nil
+		end
+	end
+end
